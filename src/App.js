f71c8b71bb3447628921ca395c5bb18887e38869
Nishant-Pall <palln6935@gmail.com>
2021-03-18 13:05:12 +0530

    Add structure for Sofa, Leather Sofa and Carpet

--- src/App.js
+++ src/App.js
@@ -7,19 +7,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Talk <code>to BuildAR's</code> Sophie.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          CleanFanatic Video Chat
-        </a>
-        <Chatbot/>
+        <Chatbot />
       </header>
     </div>
   );
--- src/Chatbot.js
+++ src/Chatbot.js
@@ -1,23 +1,24 @@
 import React, { Component } from 'react'
 import ReactPlayer from 'react-player'
-import Button from "react-bootstrap/Button"
 
 class ChatBot extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            url: "https://buildar.in/vid/1615800618.4738576.mp4",
+            url: "https://buildar.in/clean/sophie.mp4",
             isHeadClicked: false,
             transitionClass: ''
         }
         this.changeToSofa = this.changeToSofa.bind(this)
-        this.changeToLeather= this.changeToLeather.bind(this)
-        this.changeToCarpet= this.changeToCarpet.bind(this)
-        this.changeToHouse= this.changeToHouse.bind(this)
-        this.changeToKitchen= this.changeToKitchen.bind(this)
-        this.changeToBathroom= this.changeToBathroom.bind(this)
-        this.changeToFullHouse= this.changeToFullHouse.bind(this)
+        this.changeToSofa1 = this.changeToSofa1.bind(this)
+        this.changeToSofa2 = this.changeToSofa2.bind(this)
+        this.changeToLeather = this.changeToLeather.bind(this)
+        this.changeToCarpet = this.changeToCarpet.bind(this)
+        this.changeToHouse = this.changeToHouse.bind(this)
+        this.changeToKitchen = this.changeToKitchen.bind(this)
+        this.changeToBathroom = this.changeToBathroom.bind(this)
+        this.changeToFullHouse = this.changeToFullHouse.bind(this)
         this.showBot = this.showBot.bind(this)
     }
 
@@ -25,52 +26,58 @@ class ChatBot extends Component {
         document.getElementById("overlay-intro").style.display = "none"
         document.getElementById("overlay-Sofa").style.display = "block"
         this.setState({
-            url: "https://buildar.in/vid/1615800834.2182198.mp4"
+            url: "https://buildar.in/clean/sofacharges.mp4"
+        })
+    }
+    changeToSofa1() {
+        this.setState({
+            url: "https://buildar.in/clean/sofa1.mp4"
+        })
+    }
+    changeToSofa2() {
+        this.setState({
+            url: "https://buildar.in/clean/sofa2.mp4"
         })
     }
     changeToLeather() {
         document.getElementById("overlay-intro").style.display = "none"
-        document.getElementById("overlay-Sofa").style.display = "block"
+        document.getElementById("overlay-Leather-Sofa").style.display = "block"
         this.setState({
-            url: "https://buildar.in/vid/1615800834.2182198.mp4"
+            url: "https://buildar.in/clean/sofa3.mp4"
         })
     }
     changeToCarpet() {
         document.getElementById("overlay-intro").style.display = "none"
-        document.getElementById("overlay-Sofa").style.display = "block"
+        document.getElementById("overlay-Carpet").style.display = "block"
         this.setState({
-            url: "https://buildar.in/vid/1615800834.2182198.mp4"
+            url: "https://buildar.in/clean/carpetcharges.mp4"
         })
     }
     changeToHouse() {
         document.getElementById("overlay-intro").style.display = "none"
-        document.getElementById("overlay-Sofa").style.display = "block"
         this.setState({
             url: "https://buildar.in/vid/1615800834.2182198.mp4"
         })
     }
     changeToKitchen() {
         document.getElementById("overlay-intro").style.display = "none"
-        document.getElementById("overlay-Sofa").style.display = "block"
         this.setState({
             url: "https://buildar.in/vid/1615800834.2182198.mp4"
         })
     }
     changeToBathroom() {
         document.getElementById("overlay-intro").style.display = "none"
-        document.getElementById("overlay-Sofa").style.display = "block"
         this.setState({
             url: "https://buildar.in/vid/1615800834.2182198.mp4"
         })
     }
     changeToFullHouse() {
         document.getElementById("overlay-intro").style.display = "none"
-        document.getElementById("overlay-email").style.display = "block"
         this.setState({
             url: "https://buildar.in/vid/1615801043.2547426.mp4"
         })
     }
-   
+
 
     showBot() {
         if (this.state.transitionClass === '') {
@@ -93,49 +100,46 @@ class ChatBot extends Component {
         return (
             <div id="main">
                 <div id="chat-bot" className={this.state.transitionClass}>
-                    
+
                     <ReactPlayer
                         id="video-bot"
                         className="videoInsert"
                         playing={isHeadClicked}
                         controlsList="nodownload"
                         width="320px"
-                        // height="180px"
+                        height="300px"
                         // onContextMenu={(e) => e.preventDefault()}
                         /* style={{width: "160px",height:"90px"}}*/
                         url={url}
                     />
-                    
-                    
+
+
                     <div className="overlay" >
                         <div id="overlay-intro">
-                        <button className="btn btn-primary2" onClick={this.changeToSofa}>Sofa</button>
-                             <button className="btn btn-primary2" onClick={this.changeToLeather}>Leather Sofa</button>
+                            <button className="btn btn-primary2" onClick={this.changeToSofa}>Sofa</button>
+                            <button className="btn btn-primary2" onClick={this.changeToLeather}>Leather Sofa</button>
                             <button className="btn btn-primary2" onClick={this.changeToCarpet}>Carpet</button>
-                             <button className="btn btn-primary2" onClick={this.changeToHouse}>House Cleaning</button>
-                              <button className="btn btn-primary2" onClick={this.changeToKitchen}>Kitchen</button>
-                               <button className="btn btn-primary2" onClick={this.changeToBathroom}>Bathroom</button>
-                                <button className="btn btn-primary2" onClick={this.changeToFullHouse}>FullHouse</button>
+                            <button className="btn btn-primary2" onClick={this.changeToHouse}>House Cleaning</button>
+                            <button className="btn btn-primary2" onClick={this.changeToKitchen}>Kitchen</button>
+                            <button className="btn btn-primary2" onClick={this.changeToBathroom}>Bathroom</button>
+                            <button className="btn btn-primary2" onClick={this.changeToFullHouse}>FullHouse</button>
                         </div>
-                        <div id="overlay-SODA">
-                            <a type="button" className="btn btn-primary2 " href="#">Fake News</a>
-                            <a type="button" className="btn btn-primary2 " href="#">Meditation</a>
-                            <a type="button" className="btn btn-primary2" href="#">Selling Twitch</a>
+                        <div id="overlay-Sofa" style={{ display: "none" }}>
+                            <button type="button" className="btn btn-primary2 " href="#" onClick={this.changeToSofa1}>Sofa 1</button>
+                            <button type="button" className="btn btn-primary2 " href="#" onClick={this.changeToSofa2}>Sofa 2</button>
                         </div>
-                        <div id="overlay-email">
-                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+                        <div id="overlay-Leather-Sofa" style={{ display: "none" }}>
                         </div>
-                        <div id="overlay-twitter">
-                            <button type="button" className="btn" href="#twitter">
-                                <p>Here's my Twitter  </p>
-                                <i className="fa fa-twitter fa-10x" aria-hidden="true">  </i>
-                            </button>
+                        <div id="overlay-Carpet" style={{ display: "none" }}>
+                            <a type="button" className="btn btn-primary2 " href="#">Carpet 1</a>
+                            <a type="button" className="btn btn-primary2 " href="#">Carpet 2</a>
+                            <a type="button" className="btn btn-primary2" href="#">Carpet 3</a>
                         </div>
                     </div>
                 </div>
                 <div id="chathead" className="chatbot-head">
-                
-                    <button type="button" className="btn" onClick={this.showBot}>
+
+                    <button className="btn btn-primary2" onClick={this.showBot}>
                         <ReactPlayer
                             id="video-bot"
                             className="videoInsert"
@@ -143,15 +147,13 @@ class ChatBot extends Component {
                             muted
                             loop={true}
                             height="128px"
-                            width="128px"
+                            width="170px"
                             controlsList="nodownload"
                             // onContextMenu={(e) => e.preventDefault()}
-                            url="https://buildar.in/vid/1615800618.4738576.mp4"
+                            url="https://buildar.in/clean/sophie.mp4"
                         />
-                        <div className="initial_overlay">Want to talk to AI me?</div>
+                        <div className="initial_overlay">Hi!! How can I help you?</div>
                     </button>
-
-                    
                 </div>
             </div>
         )
